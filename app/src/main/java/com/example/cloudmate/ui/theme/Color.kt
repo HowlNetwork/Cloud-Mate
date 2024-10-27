7bbae730795d2ee6f4a67c43edab9dfb07c022b8
Zues1364 <giangybvn2004@gmail.com>
2024-10-27 13:16:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cloudmate/ui/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.example.cloudmate.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NavyBlue = Color(0xFF0a154b)
+val LightNavyBlue = Color(0xFF23224a)
+val Blue = Color(0xFF3092e8)
+val White = Color(0xFFFFFFFF)
+
+// Dark Theme
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E2D)
+val OnBackgroundDark = Color(0xFFE1E1E6)
+val OnSurfaceDark = Color(0xFFD0D3D4)
+val ErrorDark = Color(0xFFCF6679)
+val SecondaryVariantDark = Color(0xFF2B79C1)
+
+// Light Theme
+val LightPrimaryContainer = Color(0xFFB3C2F2)
+val LightSecondaryContainer = Color(0xFF91C9FF)
+val TertiaryLight = Color(0xFF556BFF)
+val LightBackground = Color(0xFFFFFFFF)
+val LightSurface = Color(0xFFF0F0F5)
+val OnBackgroundLight = Color(0xFF121212)
+val OnSurfaceLight = Color(0xFF23224A)
+val ErrorLight = Color(0xFFB00020)
